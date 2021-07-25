dee00a4f61be20eb660c907f0dc94d32b7cf28be
DaniilShaposhnikow <daniil_ry@hotmail.com>
2021-07-25 17:17:54 +0200

    Android-Study-Spinner
    Android-Study-Spinner

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
+import java.util.List;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+{
+    private BeerExpert expert = new BeerExpert();
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onClickFindBeer(View view)
+    {
+        TextView brands = (TextView) findViewById(R.id.brands);
+        Spinner color = (Spinner) findViewById(R.id.color);
+        String beerType = String.valueOf(color.getSelectedItem());
+        List<String> brandList = expert.getBrands(beerType);
+        StringBuilder brandsFormatted = new StringBuilder();
+        for (String brand: brandList)
+        {
+            brandsFormatted.append(brand).append('\n');
+        }
+        brands.setText(brandsFormatted);
+    }
+}
